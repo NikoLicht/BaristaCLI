8b4a53b179b7aabcb23c1ca38c895cc6471423ca
NikoLicht <artmole.niko@gmail.com>
2025-02-04 09:08:05 +0100

    updated flavour texts

--- components/drinkable.py
+++ components/drinkable.py
@@ -1,15 +1,20 @@
 from .component import Component
 from src.printing import *
+from src.grammar import Grammar
 
 class Drinkable(Component):
-    def __init__(self, taste):
+    def __init__(self, flavour_text = None):
         super().__init__()
         self.add_method(["drink"], self.drink)
-        self.taste = taste
+        self.flavour_text = flavour_text
 
     def drink(self):
+        gr = Grammar()
         if self.owner.has_state("hot"):
             say(f"You burn yourself on the boiling hot {thing(self.owner.name)}")
         else:
-            say(f"You {action("drink")} the {thing(self.owner.name)}, quite refreshing. It tastes a bit {self.taste}.")
+            if self.flavour_text is not None:
+                say(f"You {action("drink")} the {thing(self.owner.name)}. {self.flavour_text}")
+            else:
+                say(f"You {action("drink")} the {thing(self.owner.name)}, quite refreshing. While swirling it around in your mouth, you detect notes of {gr.make_list(self.owner.flavour_impact)}")
         self.owner.add_state("consumed")
--- components/edible.py
+++ components/edible.py
@@ -1,23 +1,32 @@
 from components import *
 from src.printing import *
 from src.grammar import Grammar
+from collections.abc import Callable
 
 class Edible(Component):
-    def __init__(self, taste):
+    def __init__(self, taste = None, on_eat: Callable = None):
         super().__init__()
         self.add_method(["eat"], self.eat)
         self.taste = taste
+        self.on_eat = on_eat
 
     def eat(self, triggered_action = False):
         gr = Grammar()
         if self.owner.has_state("hot"):
             say(f"You burn yourself on the boiling hot {thing(self.owner.name)}. You can't taste anything right now.")
         else:
-            say(f"You {action("eat")} the {thing(self.owner.name)}. It has quite a {gr.make_list(self.owner.flavour_impact)} aftertaste.")
-        self.owner.add_state("consumed")
+            if self.taste:
+                say(f"You {action("eat")} the {thing(self.owner.name)}. {self.taste}")
+            else:
+                say(f"You {action("eat")} the {thing(self.owner.name)}. It has quite a {gr.make_list(self.owner.flavour_impact)} aftertaste.")
 
+
+        self.owner.add_state("consumed")
         container: Container = self.owner.get_component("container")
         if container:
             container.recursive_action("eat", True)
+        if self.on_eat is not None:
+            self.on_eat()
+
 
     
